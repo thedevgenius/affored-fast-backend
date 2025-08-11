722a530a1f0e8cd372d12309d9e23d20cf642c07
rupam office <rupamdhara.1579@gmail.com>
2025-08-11 18:54:32 +0530

    Business Add

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from app.auth.routes import router as auth_router
+from app.business.routes import router as business_router
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(business_router, prefix="/business", tags=["business"])
+
 
 @app.get("/")
 async def read_root():
